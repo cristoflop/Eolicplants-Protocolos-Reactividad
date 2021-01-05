fb71c692286b736d2b1fe4e71872f17b6aeb70ef
cristoflop <cristofer.lc.99@gmail.com>
2021-01-05 15:16:17 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,53 @@
+"use strict"
+
+const express = require('express');
+const logger = require('morgan');
+const config = require("./config");
+const url = `mongodb://${config.dbConfig.host}:${config.dbConfig.databaseServerPort}/${config.dbConfig.database}`;
+const server = express();
+const mongoose = require("mongoose");
+const database = mongoose.connection;
+
+mongoose.connect(
+    url,
+    {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+)
+
+// declare routers
+
+server.use(logger(config.logging));
+server.use(express.json());
+
+// routers use
+
+server.use(middlewareNotFound);
+server.use(middlewareServerError);
+
+function middlewareNotFound(request, response) {
+    response.status(404);
+    response.json({
+        message: `${request.url} not found`
+    });
+}
+
+function middlewareServerError(error, request, response, next) {
+    response.status(500);
+    response.json(error);
+}
+
+database.on("error", function () {
+    console.error("Error al conectar con la bd");
+});
+
+database.once("open", () => {
+    server.listen(config.port, err => {
+        if (err)
+            console.error(`No se ha podido iniciar el servidor: ${err.message}`)
+        else
+            console.log(`Servidor arrancado en el puerto ${config.port}`)
+    });
+});
---
+++ config.js
@@ -0,0 +1,11 @@
+"use strict"
+
+module.exports = {
+    dbConfig: {
+        host: "localhost",
+        databaseServerPort: 27017,
+        database: "eolicplants"
+    },
+    logging: 'dev',
+    port: 3000
+}
