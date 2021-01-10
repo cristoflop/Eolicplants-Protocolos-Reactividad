8ea830be5a70c4c2d239474ae5073bc15403d019
cristoflop <cristofer.lc.99@gmail.com>
2021-01-10 01:07:29 +0100

    Changed name to publish new eoloplants in router

--- routes/eoloPlantsRouter.js
+++ routes/eoloPlantsRouter.js
@@ -6,7 +6,7 @@ const eoloPlantsController = require("../controllers/eoloPlantsController");
 
 eoloPlantsRouter.get("/eoloplants", eoloPlantsController.findAll);
 eoloPlantsRouter.get("/eoloplants/:id", eoloPlantsController.find);
-eoloPlantsRouter.post("/eoloplants", eoloPlantsController.save);
+eoloPlantsRouter.post("/eoloplants", eoloPlantsController.publish);
 eoloPlantsRouter.delete("/eoloplants", eoloPlantsController.remove);
 
 module.exports = eoloPlantsRouter;
