adb4a0a9b6b739cc52972fc7b0045573052844d9
cristoflop <cristofer.lc.99@gmail.com>
2021-01-11 01:31:51 +0100

    Updated server mysql script

--- scriptBD.sql
+++ scriptBD.sql
@@ -1,56 +1,18 @@
--- phpMyAdmin SQL Dump
--- version 4.9.2
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 10-01-2021 a las 14:29:36
--- Versión del servidor: 10.4.10-MariaDB
--- Versión de PHP: 7.3.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 DROP DATABASE IF EXISTS eolicplants;
 CREATE DATABASE IF NOT EXISTS eolicplants;
+USE eolicplants;
 
---
--- Base de datos: `eolicplants`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `eoloplants`
---
-
-CREATE TABLE `eoloplants` (
-  `id` varchar(255) NOT NULL,
-  `city` varchar(100) NOT NULL,
-  `progress` int(11) DEFAULT 0,
-  `completed` BIT  DEFAULT 0,
-  `planning` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `eoloplants`
---
-ALTER TABLE `eoloplants`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `city` (`city`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE `eoloplants`
+(
+    `id`        varchar(255) NOT NULL PRIMARY KEY,
+    `city`      varchar(100) NOT NULL UNIQUE,
+    `progress`  int(11)      DEFAULT 0,
+    `completed` BIT          DEFAULT 0,
+    `planning`  varchar(100) DEFAULT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
