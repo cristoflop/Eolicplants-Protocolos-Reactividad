0be8f9c818656a0b99b751944bd38b2ca79935cc
cristoflop <cristofer.lc.99@gmail.com>
2021-01-14 18:09:39 +0100

    Added object mapper in planner consumer

--- eolicplants-planner/src/main/java/es/urjc/eolicplants/planner/amqp/NewEoloPlantsConsumer.java
+++ eolicplants-planner/src/main/java/es/urjc/eolicplants/planner/amqp/NewEoloPlantsConsumer.java
@@ -1,15 +1,27 @@
 package es.urjc.eolicplants.planner.amqp;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import es.urjc.eolicplants.planner.amqp.dtos.NewEoloPlantDto;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class NewEoloPlantsConsumer {
 
-    @RabbitListener(queues = "newEoloPlants", ackMode = "AUTO")
-    public void received(String message) {
+    private final ObjectMapper objectMapper;
 
-        System.out.println("Message: " + message);
+    public NewEoloPlantsConsumer(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
     }
+
+    @RabbitListener(queues = "newEoloPlantsQueue", ackMode = "AUTO")
+    public void received(String newEoloPlantJson) throws JsonProcessingException {
+        // Aqui se reciben los mensajes desde el publicador y se procesan
+        System.out.println("Message: " + newEoloPlantJson);
+        NewEoloPlantDto newEoloPlantDto = this.objectMapper.readValue(newEoloPlantJson, NewEoloPlantDto.class);
+        System.out.println("Converted json: " + newEoloPlantDto);
+    }
+
 
 }
