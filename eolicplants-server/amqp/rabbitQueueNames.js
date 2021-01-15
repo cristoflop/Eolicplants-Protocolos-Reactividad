314f832ec77bc71c3ca929f13b269c17ac8af280
cristoflop <cristofer.lc.99@gmail.com>
2021-01-15 15:22:18 +0100

    Added file with rabbit queue names

---
+++ eolicplants-server/amqp/rabbitQueueNames.js
@@ -0,0 +1,6 @@
+"use strict"
+
+module.exports = {
+    eoloplantCreationRequests: "eoloplantCreationRequests",
+    eoloplantCreationProgressNotifications: "eoloplantCreationProgressNotifications"
+}
